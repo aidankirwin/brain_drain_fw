e09f912d0f03f818fd96015607446f3792190f60
aidankirwin <akirwin@uwaterloo.ca>
2026-03-19 04:08:04 -0400

    wip

--- motor.py
+++ motor.py
@@ -13,7 +13,7 @@ class Motor(threading.Thread):
         self.interval = 0.001
         self.STEP_PIN = 3
 
-        # GPIO.setup(self.STEP_PIN, GPIO.OUT)
+        GPIO.setup(self.STEP_PIN, GPIO.OUT)
 
     def run(self):
         try:
@@ -23,10 +23,10 @@ class Motor(threading.Thread):
                 else:
                     print('motor')
                     time.sleep(self.interval)
-                    # GPIO.output(self.STEP_PIN, GPIO.HIGH)
-                    # time.sleep(0.005)
-                    # GPIO.output(self.STEP_PIN, GPIO.LOW)
-                    # time.sleep(self.motor_control.delay_time)
+                    GPIO.output(self.STEP_PIN, GPIO.HIGH)
+                    time.sleep(0.005)
+                    GPIO.output(self.STEP_PIN, GPIO.LOW)
+                    time.sleep(self.motor_control.delay_time)
 
         except KeyboardInterrupt:
             print("Stopped")
--- motorcontrol.py
+++ motorcontrol.py
@@ -13,14 +13,14 @@ class MotorControl(threading.Thread):
         self.target_icp = target_icp  # Target ICP value for control logic
         self.is_draining = is_draining  # Initial drainage state is False (not draining)
 
-        # # Pin definitions
-        # self.motor_pin = 13
-        # self.servo_pin = 12
+        # Pin definitions
+        self.motor_pin = 13
+        self.servo_pin = 12
 
-        # self.servo = AngularServo(self.servo_pin, min_angle=0, max_angle=270)
-        # self.servo.detach()
+        self.servo = AngularServo(self.servo_pin, min_angle=0, max_angle=270)
+        self.servo.detach()
 
-        # self.motor = PWMOutputDevice(self.motor_pin)
+        self.motor = PWMOutputDevice(self.motor_pin)
 
     def run(self):
         while self.running:
@@ -42,9 +42,7 @@ class MotorControl(threading.Thread):
             # relationship between flow rate and step response approximately y = 0.12e^-0.0303x
             # where x = flow rate in mL/hr, and y is step delay in seconds
 
-            delay_time = icp_difference
-
-            # delay_time = 0.12 * np.e ** (-0.0303 * icp_difference)
+            delay_time = 0.12 * np.e ** (-0.0303 * icp_difference)
 
         if icp_difference <= 0 or not is_draining:
             delay_time = None  # No drainage if ICP is below target or if drainage is turned off
@@ -60,25 +58,25 @@ class MotorControl(threading.Thread):
 
     def irrigate(self):
         print('irrigating')
-        # self.servo.angle = 180
-        # time.sleep(1)
-        # self.servo.detach()
+        self.servo.angle = 180
+        time.sleep(1)
+        self.servo.detach()
 
-        # for i in range(2):
-        #         print("motor go")
-        #         self.motor.value = 200/255.0  # Convert Arduino PWM (0–255) to 0–1
-        #         time.sleep(1)
+        for i in range(2):
+                print("motor go")
+                self.motor.value = 200/255.0  # Convert Arduino PWM (0–255) to 0–1
+                time.sleep(1)
 
-        # # --- Motor OFF loop ---
-        # for i in range(1):
-        #     print("motor stop")
-        #     self.motor.value = 0
-        #     time.sleep(1)
+        # --- Motor OFF loop ---
+        for i in range(1):
+            print("motor stop")
+            self.motor.value = 0
+            time.sleep(1)
 
-        # self.servo.angle = 0
-        # time.sleep(1)
+        self.servo.angle = 0
+        time.sleep(1)
 
-        # self.servo.detach()
+        self.servo.detach()
     
     def stop(self):
         self.running = False
