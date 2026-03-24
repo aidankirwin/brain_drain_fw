b8f249d76cd11d6af2d4f82d630b2afeab579e62
aidankirwin <akirwin@uwaterloo.ca>
2026-03-24 16:14:12 -0400

    wip

--- test.py
+++ test.py
@@ -38,17 +38,17 @@ def gpio_test_loop():
     try:
         while True:
 
-            # # --- Motor ON loop ---
-            # for i in range(2):
-            #     print("dc motor start")
-            #     motor.value = 200/255.0  # Convert Arduino PWM (0–255) to 0–1
-            #     time.sleep(1)
+            # --- Motor ON loop ---
+            for i in range(2):
+                print("dc motor start")
+                motor.value = 200/255.0  # Convert Arduino PWM (0–255) to 0–1
+                time.sleep(1)
 
-            # # --- Motor OFF loop ---
-            # for i in range(1):
-            #     print("dc motor stop")
-            #     motor.value = 0
-            #     time.sleep(1)
+            # --- Motor OFF loop ---
+            for i in range(1):
+                print("dc motor stop")
+                motor.value = 0
+                time.sleep(1)
 
             # --- Stepper pulse loop ---
             for i in range(100):
@@ -58,7 +58,7 @@ def gpio_test_loop():
                 GPIO.output(step_pin, GPIO.LOW)
                 time.sleep(delay_time)
 
-            # # --- Servo movement ---
+            # --- Servo movement ---
             # time.sleep(2)
             # for i in range(2):
             #     print('servo move')
