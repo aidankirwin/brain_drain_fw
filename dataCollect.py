ed1b2edb1b5e1b85bf9a66f33f9be16ee703eec0
aidankirwin <akirwin@uwaterloo.ca>
2026-03-26 00:54:10 -0400

    wip

--- dataCollect.py
+++ dataCollect.py
@@ -66,7 +66,7 @@ class DataBuffer(threading.Thread):
         self.running = True
 
         # Timing
-        self.period = 0.01
+        self.period = 0.1
         self.fs = 100
 
         # Load calibration model
