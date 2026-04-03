99f5ffb06a17f16e2a5229aa44c5d0b20128ee4c
aidankirwin <akirwin@uwaterloo.ca>
2026-04-03 18:13:30 -0400

    wip

--- dataCollect.py
+++ dataCollect.py
@@ -194,7 +194,7 @@ class DataBuffer(threading.Thread):
             reading_arr = reading_arr * self.lc_scale + self.lc_offset
 
             # if self.load1_tare is not None:
-            #     reading_arr = reading_arr - self.load1_tare + 21.6
+            #     reading_arr = reading_arr - self.load1_tare
             #     reading_arr = np.atleast_1d(np.max([0.0, reading_arr[0]]))
 
             # if self.z_load1 is None:
@@ -220,7 +220,7 @@ class DataBuffer(threading.Thread):
             reading_arr = reading_arr * self.lc_scale + self.lc_offset
 
             if self.load2_tare is not None:
-                reading_arr = reading_arr - self.load2_tare + 20.0
+                reading_arr = reading_arr - self.load2_tare
                 reading_arr = np.atleast_1d(np.max([0.0, reading_arr[0]]))
 
             if self.z_load2 is None:
