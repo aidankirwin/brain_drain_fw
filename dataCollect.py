568e32cf97d8b229566c667d516d2e4045623e51
aidankirwin <akirwin@uwaterloo.ca>
2026-04-03 18:18:55 -0400

    wip

--- dataCollect.py
+++ dataCollect.py
@@ -219,16 +219,16 @@ class DataBuffer(threading.Thread):
             reading_arr = np.atleast_1d(reading)
             reading_arr = reading_arr * self.lc_scale + self.lc_offset
 
-            if self.load2_tare is not None:
-                reading_arr = reading_arr - self.load2_tare
-                reading_arr = np.atleast_1d(np.max([0.0, reading_arr[0]]))
+            # if self.load2_tare is not None:
+            #     reading_arr = reading_arr - self.load2_tare
+            #     reading_arr = np.atleast_1d(np.max([0.0, reading_arr[0]]))
 
-            if self.z_load2 is None:
-                self.z_load2 = signal.sosfilt_zi(self.sos_loadcell) * reading_arr
+            # if self.z_load2 is None:
+            #     self.z_load2 = signal.sosfilt_zi(self.sos_loadcell) * reading_arr
 
-            reading_arr, self.z_load2 = signal.sosfilt(
-                self.sos_loadcell, reading_arr, zi=self.z_load2
-            )
+            # reading_arr, self.z_load2 = signal.sosfilt(
+            #     self.sos_loadcell, reading_arr, zi=self.z_load2
+            # )
 
             # if time.time() - self.start_time > 5:
             #     x = self.kf_2.update(reading_arr[0])
