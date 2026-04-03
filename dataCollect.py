23f07fbaa05c0950f1734f7f473d37313991c086
aidankirwin <akirwin@uwaterloo.ca>
2026-04-03 18:27:23 -0400

    wip

--- dataCollect.py
+++ dataCollect.py
@@ -180,12 +180,12 @@ class DataBuffer(threading.Thread):
             # reading_arr = self.loaded_model['quad_model'].predict(reading_arr)
             # reading_arr = reading_arr + 1.6
 
-            # if self.z_pressure is None:
-            #     self.z_pressure = signal.sosfilt_zi(self.sos_pressure) * reading_arr
+            if self.z_pressure is None:
+                self.z_pressure = signal.sosfilt_zi(self.sos_pressure) * reading_arr
 
-            # reading_arr, self.z_pressure = signal.sosfilt(
-            #     self.sos_pressure, reading_arr, zi=self.z_pressure
-            # )
+            reading_arr, self.z_pressure = signal.sosfilt(
+                self.sos_pressure, reading_arr, zi=self.z_pressure
+            )
             return reading_arr[0]
 
         elif ch == 1:  # load cell 1
