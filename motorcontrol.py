f8747644cf33c462bd7ca42772b761d54b3e5494
aidankirwin <akirwin@uwaterloo.ca>
2026-04-05 16:06:07 -0400

    wip

--- motorcontrol.py
+++ motorcontrol.py
@@ -20,7 +20,7 @@ class MotorControl(threading.Thread):
 
         self.servo = AngularServo(self.servo_pin, min_angle=0, max_angle=180, min_pulse_width=0.0005, max_pulse_width=0.0025)
         self.servo.angle = 0
-        time.sleep(1)
+        time.sleep(2)
         self.servo.detach()
 
         self.motor = PWMOutputDevice(self.motor_pin)
